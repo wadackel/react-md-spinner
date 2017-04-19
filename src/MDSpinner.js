98e3324c5ad63135591bb2c09871e132348c9ff8
Michaël De Boey <info@michaeldeboey.be>
2017-04-19 22:21:45 +0200

    Use 'prop-types' package instead of React.PropTypes
    Closes #5

--- src/MDSpinner.js
+++ src/MDSpinner.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import exenv from "exenv";
 import CSSKeyframer from "css-keyframer";
 import { getStyles, keyframes } from "./styles";
