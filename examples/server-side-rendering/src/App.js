98c1f4f2f0c966dec5c4e028cc22419da203e736
Michaël De Boey <info@michaeldeboey.be>
2017-05-04 17:04:19 +0200

    Update yarn.lock file

--- examples/server-side-rendering/src/App.js
+++ examples/server-side-rendering/src/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, { Component } from "react";
-import MDSpinner from "../../../";
+import MDSpinner from "../../../src/MDSpinner";
 
 
 export default class App extends Component {
